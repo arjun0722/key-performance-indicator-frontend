1242fb7ddcbcc174974f42d01ef01b4ae966cfef
Luckyaremu <aremu.baba70@gmail.com>
2022-07-22 10:38:01 +0100

    kpi frontend

--- src/Config/Axios.js
+++ src/Config/Axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
   //  baseURL:"http://localhost:7000/"
-   baseURL:"https://api-qservices-tasks.azurewebsites.net"
+   baseURL:"https://api-qservices-tasks.azurewebsites.net/"
 });
 
 api.interceptors.response.use(
