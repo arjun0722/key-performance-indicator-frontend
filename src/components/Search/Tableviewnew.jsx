ae72b5d73530f521ea864318ed32ba493afba94b
Karan Singh <karansingh@qservicesit.com>
2023-03-13 12:24:58 +0530

    editTable

--- src/components/Search/Tableviewnew.jsx
+++ src/components/Search/Tableviewnew.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 import ReactHTMLTableToExcel from "react-html-table-to-excel";
 
 const Renderfirsttable = ({ val }) => {
+          console.log();
           return (
                     <>
                               <tr>
@@ -34,10 +36,107 @@ const Renderfirsttable = ({ val }) => {
           );
 };
 
-const Renderthirdtable = ({ val, thirdTable }) => {
+const Renderthirdtable = ({ val, thirdTable, ind }) => {
+          //  this state use for Appraisee Self Rating
+          const [customActualdeliveryMarks, setCustomActualdelivery] =
+                    useState("");
+          const [customOnTimeMarks, setCustomOnTimeMarks] = useState("");
+          const [customAvgCodeMarks, setCustomAvgCodeMarks] = useState("");
+          const [customReDoMarks, setCustomCustomReDoMarks] = useState("");
+          const [customBugsReportedMarks, setCustomBugsReportedMarks] =
+                    useState("");
+          const [customCriticalIssuesMarks, setCustomCriticalIssuesMarks] =
+                    useState("");
+          const [
+                    customCustomerSatisfactionMarks,
+                    setCustomCustomerSatisfactionMarks,
+          ] = useState("");
+          const [customUpskillingMarks, setCustomUpskillingMarks] =
+                    useState("");
+
+          // this states use for Appraiser Rating
+          const [customActualdeliveryMarksAr, setCustomActualdeliveryAr] =
+                    useState("");
+          const [customOnTimeMarksAr, setCustomOnTimeMarksAr] = useState("");
+          const [customAvgCodeMarksAr, setCustomAvgCodeMarksAr] = useState("");
+          const [customReDoMarksAr, setCustomCustomReDoMarksAr] = useState("");
+          const [customBugsReportedMarksAr, setCustomBugsReportedMarksAr] =
+                    useState("");
+          const [customCriticalIssuesMarksAr, setCustomCriticalIssuesMarksAr] =
+                    useState("");
+          const [
+                    customCustomerSatisfactionMarksAr,
+                    setCustomCustomerSatisfactionMarksAr,
+          ] = useState("");
+          const [customUpskillingMarksAr, setCustomUpskillingMarksAr] =
+                    useState("");
+
+          function handleOnChange(e) {
+                    // console.log(
+                    //           e.target.parentNode.parentNode.id,
+                    //           ">>>>>>>>>>>>"
+                    // );
+                    if (e.target.parentNode.parentNode.id == 0) {
+                              setCustomActualdelivery(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 1) {
+                              setCustomOnTimeMarks(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 2) {
+                              setCustomAvgCodeMarks(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 3) {
+                              setCustomCustomReDoMarks(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 4) {
+                              setCustomBugsReportedMarks(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 5) {
+                              setCustomCriticalIssuesMarks(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 6) {
+                              setCustomCustomerSatisfactionMarks(
+                                        e.target.value
+                              );
+                    }
+                    if (e.target.parentNode.parentNode.id == 7) {
+                              setCustomUpskillingMarks(e.target.value);
+                    }
+          }
+
+          function handleOnChange1(e) {
+                    console.log(e.target.value, ">>>>>>>>>>>>");
+                    if (e.target.parentNode.parentNode.id == 0) {
+                              setCustomActualdeliveryAr(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 1) {
+                              setCustomOnTimeMarksAr(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 2) {
+                              setCustomAvgCodeMarksAr(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 3) {
+                              setCustomCustomReDoMarksAr(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 4) {
+                              setCustomBugsReportedMarksAr(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 5) {
+                              setCustomCriticalIssuesMarksAr(e.target.value);
+                    }
+                    if (e.target.parentNode.parentNode.id == 6) {
+                              setCustomCustomerSatisfactionMarksAr(
+                                        e.target.value
+                              );
+                    }
+                    if (e.target.parentNode.parentNode.id == 7) {
+                              setCustomUpskillingMarksAr(e.target.value);
+                    }
+          }
+          console.log(customActualdeliveryMarks, "testtttttttttttttttts");
           return (
                     <>
-                              <tr>
+                              <tr id={ind}>
                                         <th>{val.B}</th>
                                         <td>{val.C}</td>
                                         <td>{val.D}</td>
@@ -46,16 +145,130 @@ const Renderthirdtable = ({ val, thirdTable }) => {
                                         <td>{val.G}</td>
                                         <td>{val.H}</td>
                                         <td>{val.I}</td>
-                                        <td>{val.J}</td>
+                                        <td style={{ position: "relative" }}>
+                                                  <input
+                                                            type="number"
+                                                            min="1"
+                                                            value={
+                                                                      ind === 0
+                                                                                ? customActualdeliveryMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            1
+                                                                                ? customOnTimeMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            2
+                                                                                ? customAvgCodeMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            3
+                                                                                ? customReDoMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            4
+                                                                                ? customBugsReportedMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            5
+                                                                                ? customCriticalIssuesMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            6
+                                                                                ? customCustomerSatisfactionMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            7
+                                                                                ? customUpskillingMarks
+                                                                                : 0
+                                                            }
+                                                            // value={val.J}
+                                                            style={{
+                                                                      height: "100%",
+                                                                      position: "absolute",
+                                                                      top: "0",
+                                                                      bottom: "0",
+
+                                                                      backgroundColor:
+                                                                                "#ecf0f1",
+                                                                      border: "none",
+                                                                      width: "100%",
+                                                                      fontSize: "17px",
+                                                            }}
+                                                            onChange={(e) => {
+                                                                      handleOnChange(
+                                                                                e
+                                                                      );
+                                                            }}
+                                                  />
+                                        </td>
                                         <td
                                                   style={{
                                                             backgroundColor:
                                                                       "#bf8f00",
+                                                            position: "relative",
                                                   }}
                                         >
                                                   {val.K}
                                         </td>
-                                        <td>{val.L}</td>
+                                        <td style={{ position: "relative" }}>
+                                                  <input
+                                                            type="number"
+                                                            min="1"
+                                                            value={
+                                                                      ind === 0
+                                                                                ? customActualdeliveryMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            1
+                                                                                ? customOnTimeMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            2
+                                                                                ? customAvgCodeMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            3
+                                                                                ? customReDoMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            4
+                                                                                ? customBugsReportedMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            5
+                                                                                ? customCriticalIssuesMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            6
+                                                                                ? customCustomerSatisfactionMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            7
+                                                                                ? customUpskillingMarksAr
+                                                                                : 0
+                                                            }
+                                                            style={{
+                                                                      height: "100%",
+                                                                      position: "absolute",
+                                                                      top: "0",
+                                                                      bottom: "0",
+
+                                                                      backgroundColor:
+                                                                                "#ecf0f1",
+                                                                      border: "none",
+                                                                      width: "100%",
+                                                                      fontSize: "17px",
+                                                            }}
+                                                            // value={val.L}
+                                                            onChange={(e) => {
+                                                                      handleOnChange1(
+                                                                                e
+                                                                      );
+                                                            }}
+                                                  />
+                                                  {/* {val.L} */}
+                                        </td>
                                         <td
                                                   style={{
                                                             backgroundColor:
@@ -268,7 +481,7 @@ const Tableviewnew = ({ fileData, TaskwiseMarks, email }) => {
                                                   return (
                                                             <>
                                                                       <Renderthirdtable
-                                                                                key={
+                                                                                ind={
                                                                                           ind
                                                                                 }
                                                                                 val={
@@ -379,8 +592,37 @@ const Tableviewnew = ({ fileData, TaskwiseMarks, email }) => {
                                                             </>
                                                   );
                                         })}
+                                        <td style={{ border: "none" }}></td>
+                                        <td style={{ border: "none" }}></td>
+                                        <td style={{ border: "none" }}></td>
+                                        <td style={{ border: "none" }}></td>
+                                        <td style={{ border: "none" }}></td>
+                                        <td style={{ border: "none" }}></td>
+                                        <td style={{ border: "none" }}></td>
+                                        <td style={{ border: "none" }}></td>
 
+                                        <div>
+                                                  <Button
+                                                            variant="contained"
+                                                            style={{
+                                                                      width: "100%",
+                                                                      minWidth: "120px",
+                                                            }}
+
+                                                            //   onClick={() => {
+                                                            //             setstartDate(
+                                                            //                       customdate[0]
+                                                            //             );
+                                                            //             setlastDate(
+                                                            //                       customdate[1]
+                                                            //             );
+                                                            //   }}
+                                                  >
+                                                            Submit KPI
+                                                  </Button>
+                                        </div>
                                         <tr style={{ height: "100px" }}></tr>
+
                                         {fifthTable.map((val, index) => {
                                                   return (
                                                             <>
