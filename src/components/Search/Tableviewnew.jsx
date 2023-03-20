4704f2474c4e60d8daa3d10fed1b1b697d00b020
Karan Singh <karansingh@qservicesit.com>
2023-03-20 18:28:39 +0530

    conditions

--- src/components/Search/Tableviewnew.jsx
+++ src/components/Search/Tableviewnew.jsx
@@ -459,47 +459,63 @@ const RenderTestTable = ({
 }) => {
           //state to maintain target values
 
-          const [actualDelivery, setActualDelivery] = useState(0);
-          const [onTime, setOnTime] = useState(0);
-          const [critical, setCritical] = useState(0);
+          const [actualDelivery, setActualDelivery] = useState(
+                    ind === 0 ? val.Target : 0
+          );
+          const [onTime, setOnTime] = useState(ind === 1 ? val.Target : 0);
+          const [critical, setCritical] = useState(ind === 5 ? val.Target : 0);
 
           const [loginUser, setLoginUser] = useState(
                     localStorage.getItem(ACCESS_TOKEN.USER_EMAIL)
           );
 
           //  this state use for Appraisee Self Rating
-          const [customActualdeliveryMarks, setCustomActualdelivery] =
-                    useState("");
-          const [customOnTimeMarks, setCustomOnTimeMarks] = useState("");
-          const [customAvgCodeMarks, setCustomAvgCodeMarks] = useState("");
-          const [customReDoMarks, setCustomCustomReDoMarks] = useState("");
+          const [customActualdeliveryMarks, setCustomActualdelivery] = useState(
+                    ind === 0 ? val.AppraiseeSelfRating : 0
+          );
+          const [customOnTimeMarks, setCustomOnTimeMarks] = useState(
+                    ind === 1 ? val.AppraiseeSelfRating : 0
+          );
+          const [customAvgCodeMarks, setCustomAvgCodeMarks] = useState(
+                    ind === 2 ? val.AppraiseeSelfRating : 0
+          );
+          const [customReDoMarks, setCustomCustomReDoMarks] = useState(
+                    ind === 3 ? val.AppraiseeSelfRating : 0
+          );
           const [customBugsReportedMarks, setCustomBugsReportedMarks] =
-                    useState("");
+                    useState(ind === 4 ? val.AppraiseeSelfRating : 0);
           const [customCriticalIssuesMarks, setCustomCriticalIssuesMarks] =
-                    useState("");
+                    useState(ind === 5 ? val.AppraiseeSelfRating : 0);
           const [
                     customCustomerSatisfactionMarks,
                     setCustomCustomerSatisfactionMarks,
-          ] = useState("");
-          const [customUpskillingMarks, setCustomUpskillingMarks] =
-                    useState("");
+          ] = useState(ind === 6 ? val.AppraiseeSelfRating : 0);
+          const [customUpskillingMarks, setCustomUpskillingMarks] = useState(
+                    ind === 7 ? val.AppraiseeSelfRating : 0
+          );
 
           // this states use for Appraiser Rating
           const [customActualdeliveryMarksAr, setCustomActualdeliveryAr] =
-                    useState("");
-          const [customOnTimeMarksAr, setCustomOnTimeMarksAr] = useState("");
-          const [customAvgCodeMarksAr, setCustomAvgCodeMarksAr] = useState("");
-          const [customReDoMarksAr, setCustomCustomReDoMarksAr] = useState("");
+                    useState(ind === 0 ? val.AppraiserRating : 0);
+          const [customOnTimeMarksAr, setCustomOnTimeMarksAr] = useState(
+                    ind === 1 ? val.AppraiserRating : 0
+          );
+          const [customAvgCodeMarksAr, setCustomAvgCodeMarksAr] = useState(
+                    ind === 2 ? val.AppraiserRating : 0
+          );
+          const [customReDoMarksAr, setCustomCustomReDoMarksAr] = useState(
+                    ind === 3 ? val.AppraiserRating : 0
+          );
           const [customBugsReportedMarksAr, setCustomBugsReportedMarksAr] =
-                    useState("");
+                    useState(ind === 4 ? val.AppraiserRating : 0);
           const [customCriticalIssuesMarksAr, setCustomCriticalIssuesMarksAr] =
-                    useState("");
+                    useState(ind === 5 ? val.AppraiserRating : 0);
           const [
                     customCustomerSatisfactionMarksAr,
                     setCustomCustomerSatisfactionMarksAr,
-          ] = useState("");
+          ] = useState(ind === 6 ? val.AppraiserRating : 0);
           const [customUpskillingMarksAr, setCustomUpskillingMarksAr] =
-                    useState("");
+                    useState(ind === 7 ? val.AppraiserRating : 0);
           console.log(val, "''''''''''''''''''''");
           console.log(val.Category, "}}}}}}}}}}}}}}}}}}");
           console.log(com[ind], "COM");
@@ -681,9 +697,7 @@ const RenderTestTable = ({
                                                   <input
                                                             type="number"
                                                             min="0"
-                                                            value={
-                                                                      val.AppraiseeSelfRating
-                                                            }
+                                                            value={val.Target}
                                                             style={{
                                                                       height: "100%",
                                                                       position: "absolute",
@@ -709,38 +723,37 @@ const RenderTestTable = ({
                                                             type="number"
                                                             min="1"
                                                             value={
-                                                                      val.AppraiserRating
-                                                                      // ind === 0
-                                                                      //           ? customActualdeliveryMarks
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       1
-                                                                      //           ? customOnTimeMarks
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       2
-                                                                      //           ? customAvgCodeMarks
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       3
-                                                                      //           ? customReDoMarks
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       4
-                                                                      //           ? customBugsReportedMarks
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       5
-                                                                      //           ? customCriticalIssuesMarks
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       6
-                                                                      //           ? customCustomerSatisfactionMarks
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       7
-                                                                      //           ? customUpskillingMarks
-                                                                      //           : 0
+                                                                      ind === 0
+                                                                                ? customActualdeliveryMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            1
+                                                                                ? customOnTimeMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            2
+                                                                                ? customAvgCodeMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            3
+                                                                                ? customReDoMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            4
+                                                                                ? customBugsReportedMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            5
+                                                                                ? customCriticalIssuesMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            6
+                                                                                ? customCustomerSatisfactionMarks
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            7
+                                                                                ? customUpskillingMarks
+                                                                                : 0
                                                             }
                                                             style={{
                                                                       height: "100%",
@@ -776,38 +789,39 @@ const RenderTestTable = ({
                                                             type="number"
                                                             min="1"
                                                             value={
-                                                                      com[ind].L
-                                                                      // ind === 0
-                                                                      //           ? customActualdeliveryMarksAr
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       1
-                                                                      //           ? customOnTimeMarksAr
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       2
-                                                                      //           ? customAvgCodeMarksAr
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       3
-                                                                      //           ? customReDoMarksAr
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       4
-                                                                      //           ? customBugsReportedMarksAr
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       5
-                                                                      //           ? customCriticalIssuesMarksAr
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       6
-                                                                      //           ? customCustomerSatisfactionMarksAr
-                                                                      //           : 0 ||
-                                                                      //             ind ===
-                                                                      //                       7
-                                                                      //           ? customUpskillingMarksAr
-                                                                      //           : 0
+                                                                      // val.AppraiserRating
+                                                                      // com[ind].L
+                                                                      ind === 0
+                                                                                ? customActualdeliveryMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            1
+                                                                                ? customOnTimeMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            2
+                                                                                ? customAvgCodeMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            3
+                                                                                ? customReDoMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            4
+                                                                                ? customBugsReportedMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            5
+                                                                                ? customCriticalIssuesMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            6
+                                                                                ? customCustomerSatisfactionMarksAr
+                                                                                : 0 ||
+                                                                                  ind ===
+                                                                                            7
+                                                                                ? customUpskillingMarksAr
+                                                                                : 0
                                                             }
                                                             style={{
                                                                       height: "100%",
